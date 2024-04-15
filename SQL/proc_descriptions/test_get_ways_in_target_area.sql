5d34bfd3f5470478bc283a1d0d791d1b4494403d
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2024-04-15 10:28:55 +0200

    Modified mob_group_runtests() functions to rollback changes made in startup testing functions

--- SQL/proc_descriptions/test_get_ways_in_target_area.sql
+++ SQL/proc_descriptions/test_get_ways_in_target_area.sql
@@ -6,6 +6,8 @@ CREATE OR REPLACE FUNCTION startup_get_ways_in_target_area()
 RETURNS VOID AS $$
 BEGIN
     RAISE NOTICE 'execution of startup_get_ways_in_target_area() started';
+    INSERT INTO debug.areas(id, name, description, geom)
+    VALUES (50, 'no target area', 'no target area', ST_GeomFromText('POLYGON((0 0, 0 1, 1 1, 1 0, 0 0))', 4326));
 END;
 $$ LANGUAGE plpgsql;
 
@@ -14,22 +16,22 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION setup_get_ways_in_target_area_no_target_area()
 RETURNS SETOF TEXT AS $$
 BEGIN
-    -- call startup
     RAISE NOTICE 'execution of setup_get_ways_in_target_area_no_target_area() started';
+    INSERT INTO debug.areas(id, name, description, geom)
+    VALUES (51, 'target area', 'target area', ST_GeomFromText('POLYGON((0 0, 0 1, 1 1, 1 0, 0 0))', 4326));
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION test_get_ways_in_target_area_no_target_area()
 RETURNS SETOF TEXT AS $$
 BEGIN
-    -- call setup
     RAISE NOTICE 'execution of test_get_ways_in_target_area_no_target_area() started';
 --     RETURN QUERY SELECT * FROM get_ways_in_target_area(0);
     RETURN NEXT pass('no records on return, when there is no target_area');
 END
 $$ LANGUAGE plpgsql;
 
--- tests that shouldn't be executed in runtests('get_ways_in_target_area_no_target_area')
+-- tests that shouldn't be executed in mob_group_runtests('_get_ways_in_target_area')
 
 CREATE OR REPLACE FUNCTION startup_assign_average_speed_to_ways()
 RETURNS VOID AS $$
--- SQL/proc_descriptions/testing_extension.sql
+++ SQL/proc_descriptions/testing_extension.sql
@@ -1,7 +1,21 @@
 -- Mobility group runtests(): alternative for pgTap.runtests()
 -- Mobility group runtests()
 -- TODO come up with adequate naming or leave it like that
-create function mob_group_runtests(name, text) returns SETOF text
+
+
+-- Function: mob_group_runtests(name, text)
+-- This function is used to run a group of tests.
+-- Input parameters: name - the name of the schema to look for the test functions;
+--                   text - the pattern to match the names of the functions to run.
+-- Flow:
+-- 1. Run alphabetically ordered startup functions once, which match the pattern "startup" + $2.
+-- 2. Enter a loop:
+--  2.1. Run alphabetically ordered setup functions once, which match the pattern "setup" + $2.
+--  2.2. Run alphabetically ordered test functions once, which match the pattern $2.
+--  2.3. Run alphabetically ordered teardown functions once, which match the pattern "teardown" + $2.
+-- 3. Run alphabetically ordered shutdown functions once, which match the pattern "shutdown" + $2.
+-- 4. Return the results of the tests.
+create or replace function mob_group_runtests(name, text) returns SETOF text
     language plpgsql
 as
 $$
@@ -17,20 +31,43 @@ DECLARE
     exclude_pattern TEXT := '^(' || startup || '|' || shutdown || '|' || setup || '|' || teardown || ')';
     result_record RECORD;
 BEGIN
-    FOR result_record IN
-        SELECT * FROM _runner(
-            findfuncs( $1, startup_pattern ),
-            findfuncs( $1, shutdown_pattern ),
-            findfuncs( $1, setup_pattern ),
-            findfuncs( $1, teardown_pattern ),
-            findfuncs( $1, $2, exclude_pattern )
-        )
-    LOOP
-        RETURN NEXT result_record;
-    END LOOP;
+    -- A little note about raising exception in this function, it seems that `_runner` function
+    -- does not raise an exception, instead catching it and returning it as a record, so under
+    -- this assumption we raise "division by zero" AKA "22012" exception to rollback the transaction.
+    BEGIN -- begin transaction for later rollback
+        FOR result_record IN
+            SELECT * FROM _runner(
+                findfuncs( $1, startup_pattern ),
+                findfuncs( $1, shutdown_pattern ),
+                findfuncs( $1, setup_pattern ),
+                findfuncs( $1, teardown_pattern ),
+                findfuncs( $1, $2, exclude_pattern )
+            )
+        LOOP
+            RETURN NEXT result_record;
+        END LOOP;
+
+        -- Raise the exception
+        RAISE EXCEPTION '__TAP_ROLLBACK__' USING ERRCODE = '22012';
+    EXCEPTION
+        WHEN SQLSTATE '22012' THEN
+            -- Catch the exception to prevent it from propagating
+            NULL; -- Do nothing
+    END;
 END;
 $$;
 
+-- Function: mob_group_runtests(text)
+-- This function is used to run a group of tests.
+-- Input parameters: text - the pattern to match the names of the functions to run.
+-- Flow:
+-- 1. Run alphabetically ordered startup functions once, which match the pattern "startup" + $1.
+-- 2. Enter a loop:
+--  2.1. Run alphabetically ordered setup functions once, which match the pattern "setup" + $1.
+--  2.2. Run alphabetically ordered test functions once, which match the pattern $1.
+--  2.3. Run alphabetically ordered teardown functions once, which match the pattern "teardown" + $1.
+-- 3. Run alphabetically ordered shutdown functions once, which match the pattern "shutdown" + $1.
+-- 4. Return the results of the tests.
 create or replace function mob_group_runtests(text) returns SETOF text
     language plpgsql
 as
@@ -47,6 +84,10 @@ DECLARE
     exclude_pattern TEXT := '^(' || startup || '|' || shutdown || '|' || setup || '|' || teardown || ')';
     result_record RECORD;
 BEGIN
+    -- A little note about raising exception in this function, it seems that `_runner` function
+    -- does not raise an exception, instead catching it and returning it as a record, so under
+    -- this assumption we raise "division by zero" AKA "22012" exception to rollback the transaction.
+    BEGIN -- begin transaction for later rollback
     FOR result_record IN
         SELECT * FROM _runner(
             findfuncs( startup_pattern ),
@@ -58,5 +99,12 @@ BEGIN
     LOOP
         RETURN NEXT result_record;
     END LOOP;
+        -- Raise the exception
+        RAISE EXCEPTION '__TAP_ROLLBACK__' USING ERRCODE = '22012';
+    EXCEPTION
+        WHEN SQLSTATE '22012' THEN
+            -- Catch the exception to prevent it from propagating
+            NULL; -- Do nothing
+    END;
 END;
 $$;
