ef0ec3b2473b8b9512f5626ca31863e3899e9144
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2024-06-24 09:59:38 +0200

    Replaced 4spaces with tabulation, cleaned debug comments

--- SQL/proc_descriptions/procedure_compute_speeds_from_neigborhood_segments.sql
+++ SQL/proc_descriptions/procedure_compute_speeds_from_neigborhood_segments.sql
@@ -14,10 +14,10 @@ BEGIN
 	RAISE NOTICE 'selecting target ways';
 
 	-- 1.1 Create view target_ways
-  EXECUTE format($target_ways_view$
+	EXECUTE format($target_ways_view$
 	CREATE MATERIALIZED VIEW target_ways AS
 	SELECT ways.* FROM ways JOIN areas ON areas.id = %L AND st_intersects(areas.geom, ways.geom)
-  $target_ways_view$, target_area_id);
+	$target_ways_view$, target_area_id);
 
 	-- 1.2 Add index on target_ways(id)
 	CREATE INDEX target_ways_id_idx ON target_ways(id);
@@ -26,7 +26,7 @@ BEGIN
 	RAISE NOTICE 'creating node segments view';
 
 	-- 2.1 Create a view of node segments for the target ways
-  EXECUTE format($node_segments_view$
+	EXECUTE format($node_segments_view$
 	CREATE MATERIALIZED VIEW node_segments AS
 		SELECT
 			from_nodes_ways.id AS from_id,
@@ -47,7 +47,7 @@ BEGIN
 		JOIN nodes from_nodes ON from_nodes_ways.node_id = from_nodes.id
 		JOIN nodes to_nodes ON to_node_ways.node_id = to_nodes.id
 		WHERE nodes_ways_speeds.to_node_ways_id IS NULL
-    $node_segments_view$, target_area_srid);
+		$node_segments_view$, target_area_srid);
 
 	-- 2.2 Add index on geom (with the help of Generalized Search Tree)
 	CREATE INDEX node_segments_geom_idx
@@ -82,7 +82,7 @@ BEGIN
 	-- 4.1 Update count to current number of records in speed_segment_data
 	assigned_segments_count = (SELECT count(1) FROM speed_segment_data);
 
-	RAISE NOTICE '% segments with assigned speed  found in target area', assigned_segments_count;
+	RAISE NOTICE '% segments with assigned speed	found in target area', assigned_segments_count;
 
 	-- 5.1 Create a view to count assigned segments in the target area
 	CREATE VIEW assigned_segments_in_target_area AS
