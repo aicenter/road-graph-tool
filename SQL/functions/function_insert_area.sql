2c56d292bf60973a1fbc9efaebb9e09075d661de
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2024-07-03 13:34:15 +0200

    Replaced insert_area overloading to optinoal args

--- SQL/functions/function_insert_area.sql
+++ SQL/functions/function_insert_area.sql
@@ -12,30 +12,20 @@
 -- Author: Vladyslav Zlochevskyi
 -- Date: 2024
 ------------------------------------------------------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION insert_area(integer, varchar, varchar, json) RETURNS VOID AS
+CREATE OR REPLACE FUNCTION insert_area(
+    id INTEGER DEFAULT NULL,
+    name VARCHAR,
+    description VARCHAR DEFAULT NULL,
+    geom JSON
+) RETURNS VOID AS
 $$
 BEGIN
-    INSERT INTO areas(id, name, description, geom) VALUES($1, $2, $3, st_geomfromgeojson($4));
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION insert_area(varchar, varchar, json) RETURNS VOID AS
-$$
-BEGIN
-  PERFORM insert_area(NULL, $1, $2, $3);
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION insert_area(integer, varchar, json) RETURNS VOID AS
-$$
-BEGIN
-  PERFORM insert_area($1, $2, NULL, $3);
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION insert_area(varchar, json) RETURNS VOID AS
-$$
-BEGIN
-  PERFORM insert_area(NULL, $1, NULL, $2);
+    INSERT INTO areas(id, name, description, geom)
+    VALUES (
+        COALESCE(id, DEFAULT),
+        name,
+        description,
+        st_geomfromgeojson(geom)
+    );
 END;
 $$ LANGUAGE plpgsql;
