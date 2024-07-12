e740cd045f4693a6ff35ee3f8e81f733c20ed700
Domi Sidlova <domisidlova@gmail.com>
2024-07-12 18:51:28 +0200

    Migrate importer folder to the project folder structure

--- process_osm.sh
+++ process_osm.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+# File path to the config file
+CONFIG_FILE="config.ini"
+
 # Function to display usage information
 function display_help {
     echo "Usage: $0 [tag] [input_file]"
     echo "  Tag: "
     echo "      -d       : Display OSM file"
     echo "      -i       : Display information about OSM file"
+    echo "      -ie      : Display extended information about OSM file"
     echo "Usage: $0 [tag] [input_file] -o [output_file]"
     echo "  Tag: "
-    echo "      -r       : Renumber object IDs in OSM file"
+    echo "     -h/--help : Display this help message"
+    echo "     -r       : Renumber object IDs in OSM file"
     echo "               (Requires specifying output file with '-o' tag)"
-    echo "      -s       : Sort OSM file based on IDs"
+    echo "     -s       : Sort OSM file based on IDs"
     echo "               (Requires specifying output file with '-o' tag)"
-    echo "      -h       : Display this help message"
+    echo "Usage: $0 -f [input_file] [style_file_path]"
+    echo "  Tag: "
+    echo "     -f       : Import OSM file to PostgreSQL database using osm2pgsql with the specified style file"
+    echo "               (Optional: specify style file path - default.lua is used otherwise)"
     exit 0
 }
 
@@ -33,14 +41,14 @@ function check_extension {
     fi
 }
 
-# Check if the script is called with -h
-if [ "$1" == "-h" ]; then
+# Check if the script is called with -h/--help
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
     display_help
 fi
 
 # Check if the correct number of arguments is provided
 if [ $# -lt 2 ]; then
-    echo "Error: Insufficient arguments. Use \"$0 -h\" for hint."
+    echo "Error: Insufficient arguments. Use \"$0 -h/--help\" for hint."
     exit 1
 fi
 
@@ -56,6 +64,12 @@ fi
 
 # Check the input file extension
 check_extension "$file_name"
+
+# Get value of given key from the config file
+get_config_value() {
+    key=$1
+    grep -E "^${key}" "$CONFIG_FILE" | sed -E "s/^[^=]*= *//"
+}
 
 # Run command based on provided tag
 case "$tag" in
@@ -76,7 +90,7 @@ case "$tag" in
         fi
         output_file=$4
         # Check the output file extension
-        # check_extension "$output_file"
+        check_extension "$output_file"
 
         osmium renumber "$file_name" -o "$output_file"
         ;;
@@ -91,12 +105,24 @@ case "$tag" in
         fi
         output_file=$4
         # Check the output file extension
-        # check_extension "$output_file"
+        check_extension "$output_file"
 
         osmium sort "$file_name" -o "$output_file"
         ;;
+    -f)
+        if [ "$4"]; then
+            style_file_path=$4
+        fi
+        input_file=$3
+        style_file_path="resources/lua_styles/default.lua"
+        db_username=$(get_config_value "username")
+        db_host=$(get_config_value "db_host")
+        db_name=$(get_config_value "db_name")
+        db_server_port=$(get_config_value "db_server_port")
+        osm2pgsql -d "$db_name" -U "$db_username" -W -H "$db_host" -P "$db_server_port" --output=flex -S "$style_file_path" "$input_file" -x
+        ;;
     *)
-        echo "Invalid tag. Valid tags are: -d (display file), -i (info), -s (sort), -r (renumber), -h (help) ."
+        echo "Invalid tag. Use \"$0 -h/--help\" for hint."
         exit 1
         ;;
 esac
