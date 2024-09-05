4bd2ec55fb43f5083f28d5497fe25a27f18445d8
XrayT3 <sha0006@yandex.ru>
2024-09-05 15:06:07 +0500

    add test for dm table

--- python/tests/test_distance_matrix_generator.py
+++ python/tests/test_distance_matrix_generator.py
@@ -1,5 +1,6 @@
 import pytest
 import logging
+import pandas as pd
 import geopandas as gpd
 
 from roadgraphtool.distance_matrix_generator import generate_dm, _set_config_defaults
@@ -54,20 +55,17 @@ def test_set_config_defaults_complex():
 @pytest.fixture
 def sample_nodes():
     return gpd.GeoDataFrame({
-        'db_id': [1, 2],
-        'x': [0.0, 1.0],
-        'y': [0.0, 0.0],
+        'db_id': [10, 20, 30],
     })
 
 
 @pytest.fixture
 def sample_edges():
     return gpd.GeoDataFrame({
-        'u': [1.0, -1.0],
-        'v': [0.0, 0.0],
-        'db_id_from': [1, 2],
-        'db_id_to': [2, 1],
-        'length': [1.0, 1.0]
+        'u': [0, 1, 0, 1],
+        'v': [1, 2, 2, 0],
+        'length': [100, 100, 140, 100],
+        'speed': [36, 36, 36, 36]
     })
 
 
@@ -113,5 +111,19 @@ def test_generate_dm_skip_generation(caplog, config, sample_nodes, sample_edges)
 def test_generate_dm_travel_time(config, sample_nodes, sample_edges):
     generate_dm(config, sample_nodes, sample_edges)
     assert 'travel_time' in sample_edges.columns
-    assert sample_edges['travel_time'].at[0] == 0
-    assert sample_edges['travel_time'].at[1] == 0
+    assert sample_edges['travel_time'].at[0] == 10
+    assert sample_edges['travel_time'].at[1] == 10
+
+
+def test_generate_dm(config, sample_nodes, sample_edges, tmp_path):
+
+    generate_dm(config, sample_nodes, sample_edges)
+
+    df = pd.read_csv((tmp_path / 'dm.csv'))
+
+    expected_df = pd.DataFrame({
+        '0': [10, 4294967295],
+        '10': [0, 4294967295],
+        '14': [10, 0]})
+
+    pd.testing.assert_frame_equal(df, expected_df)
